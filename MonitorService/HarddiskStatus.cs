4e452e59a8cecf6b8d7c7ef960c81eba061c227a
Patrick Maagaard Hjorth Laursen <patrick.laursen@gmail.com>
2017-11-29 12:20:20 +0100

    harddisk done

--- MonitorService/HarddiskStatus.cs
+++ MonitorService/HarddiskStatus.cs
@@ -21,9 +21,12 @@ namespace MonitorService
                 double TotalDiskSpaceGB = 0;
                 foreach (var freeSpace in resultTotalAmountDiskSpace)
                 {
+                    //The size of the disk is calculated as followed: TotalCylinder * TracksPerCylinder * SectorsPerTrack * BytesInEachSector
+                    //So for a 512GB disk it is: 62260 * 255 * 63 * 512 = 512GB of disk storage total
                     TotalDiskSpaces = Convert.ToDouble(freeSpace["Size"]); //Get the property Size
                     TotalDiskSpaceGB = Math.Round(TotalDiskSpaces / 1000000000,0);
                     return TotalDiskSpaceGB;
+                    //The return value will not match what windows task manager shows, due to reserved hidden system files.
                 }
 
             }
@@ -39,6 +42,8 @@ namespace MonitorService
         {
             try
             {
+                //Here we get the amount of free disk space of the total amount on the disk.
+                //If you have a total disk space of 512GB, we get the used space of the 512GB here
                 ObjectQuery FreeSpaceOnDisk = new ObjectQuery("SELECT * FROM Win32_LogicalDisk");
                 ManagementObjectSearcher searcherFreeDiskSpace = new ManagementObjectSearcher(FreeSpaceOnDisk);
                 ManagementObjectCollection resultFreeDiskSpace = searcherFreeDiskSpace.Get();
